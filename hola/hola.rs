f56e00c1d88d206e21f94476e1707fddd8824833
ATG-5222 <A01275222@tec.mx>
2024-02-08 21:13:00 -0600

    Añadir Hello world

---
+++ hola/hola.rs
@@ -0,0 +1,16 @@
+//Comentario de linea
+/* Comentario de bloque */
+
+/*
+    Como compilar:
+    rustc "Nombre del programa"
+*/
+
+/*
+    Como ejecutar:
+    ./"Nombre del programa"
+*/
+
+fn main () {
+    println!("¡Hola mundo!")
+}
